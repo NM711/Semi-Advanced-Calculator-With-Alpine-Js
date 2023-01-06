c099973fd37dff724804aac24af44a849040c49c
Nathan <nm7994918@gmail.com>
2023-01-06 14:19:52 -0800

    calculator

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "jet": "#2F2F2F"
+      }
+    },
+  },
+  plugins: [],
+}
